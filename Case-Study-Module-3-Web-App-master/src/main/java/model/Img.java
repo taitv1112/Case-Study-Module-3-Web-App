4a0a857ee51ea6da35b590c461946eca6b694589
buivietthang <thangnov92@gmail.com>
2022-01-02 21:37:56 +0700

    model

---
+++ Case-Study-Module-3-Web-App-master/src/main/java/model/Img.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Img {
+    private int idImg;
+    private int idProduct;
+    private String img;
+
+    public Img() {
+    }
+
+    public Img(int idImg, int idProduct, String img) {
+        this.idImg = idImg;
+        this.idProduct = idProduct;
+        this.img = img;
+    }
+
+    public int getIdImg() {
+        return idImg;
+    }
+
+    public void setIdImg(int idImg) {
+        this.idImg = idImg;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
